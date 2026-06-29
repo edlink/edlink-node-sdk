7a414d8785893b7660093854e272a921ac02c9fa
Tenari <djz24@case.edu>
2026-06-29 17:38:41 -0500

    event v1

--- src/common/audit_event.ts
+++ src/common/audit_event.ts
@@ -1,69 +1,45 @@
-import {
-  AuditEvent,
-  AuditSchemaAction,
-  BearerTokenAPI,
-  CRUDXType,
-  RequestOptionsBase,
-  RequestOptionsGet,
-  RequestOptionsPaging,
-  RequestOptionsPost,
-} from "../types";
+import { Actor, AuditEvent, BearerTokenAPI, Context, RequestOptionsGet, RequestOptionsPaging, RequestOptionsPost, Scope, Target } from '../types';
 
 export class AuditEvents {
-  constructor(private api: BearerTokenAPI) {
-    if (api.api !== "audit") {
-      throw new Error(
-        "The Audit Logs API only works with the Application TokenSetType",
-      );
+    constructor(private api: BearerTokenAPI) {
+        if (api.api !== 'audit') {
+            throw new Error('The Audit Logs API only works with the Application TokenSetType');
+        }
     }
-  }
 
-  /**
-   * Fetches a single AuditEvent by ID.
-   * @param event_id The UUID of the AuditEvent, or the 'action' string
-   * @returns The requested AuditEvent
-   */
-  fetch(
-    event_id: string,
-    options: RequestOptionsGet = {},
-  ): Promise<AuditEvent> {
-    return this.api.request(`/events/${event_id}`, options);
-  }
+    /**
+     * Paginates through all Events within a given scope.
+     * @param scope_type The type of scope, either `integration` or `institution`
+     * @param scope_id The UUID of the scope
+     * @param options Provide a `limit` for the max number of results
+     */
+    async *list(scope_type: string, scope_id: string, options: RequestOptionsPaging = {}): AsyncGenerator<AuditEvent> {
+        yield* this.api.paginate<AuditEvent>(`/events/${scope_type}/${scope_id}`, options);
+    }
 
-  /**
-   * Paginates through all classes that the user has access to.
-   * @param options Provide a `limit` for the max number of results
-   */
-  async *list(
-    scope_id: string,
-    scope_type: string,
-    options: { limit?: number } = {},
-  ): AsyncGenerator<AuditEvent> {
-    yield* this.api.paginate<AuditEvent>(
-      `/events/${scope_type}/${scope_id}`,
-      options,
-    );
-  }
+    /**
+     * Fetches a single Event by ID.
+     * @param event_id The UUID of the Event
+     * @returns The requested Event
+     */
+    fetch(event_id: string, options: RequestOptionsGet = {}): Promise<AuditEvent> {
+        return this.api.request(`/events/${event_id}`, options);
+    }
 
-  /**
-   * Create the Event for a given 'action'.
-   * @param class_id The UUID of the class
-   * @returns The requested class
-   */
-  create(
-    action: AuditEventAction,
-    data: any = {},
-    validation_level: "strict" | "lax" = "lax",
-    options: RequestOptionsPost = {},
-  ): Promise<AuditEvent> {
-    return this.api.request({
-      url: `/schemas`,
-      method: "POST",
-      data: {
-        action,
-        validation_level,
-        data,
-      },
-    }, options);
-  }
+    /**
+     * Records a new Event.
+     * @param event The Event body to record
+     * @param options Optional request options
+     * @returns The recorded Event's assigned ID
+     */
+    create(
+        event: { actor: Actor; action: string; targets: Target[]; scope: Scope; context?: Context; data?: any },
+        options: RequestOptionsPost = {},
+    ): Promise<Pick<AuditEvent, 'id'>> {
+        return this.api.request({
+            url: '/events',
+            method: 'POST',
+            data: event,
+        }, options);
+    }
 }
