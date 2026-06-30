022be90d31d26a50b74cfe698d79cf79a4619991
Tenari <djz24@case.edu>
2026-06-30 11:06:26 -0500

    reset common.ts to undo the pile of prettier changes

--- src/types/common.ts
+++ src/types/common.ts
@@ -3,76 +3,78 @@
  * @enum {string}
  */
 export enum Subject {
-  // CEDS Subjects
-  LanguageArts = "CEDS.01", // EnglishLanguageAndLiterature in CEDS
-  Mathematics = "CEDS.02",
-  LifeAndPhysicalSciences = "CEDS.03",
-  SocialSciencesAndHistory = "CEDS.04",
-  VisualAndPerformingArts = "CEDS.05",
-  ReligiousEducationAndTheology = "CEDS.07",
-  PhysicalHealthAndSafetyEducation = "CEDS.08",
-  MilitaryScience = "CEDS.09",
-  InformationTechnology = "CEDS.10",
-  CommunicationAndAudioVisualTechnology = "CEDS.11",
-  BusinessAndMarketing = "CEDS.12",
-  Manufacturing = "CEDS.13",
-  HealthCareSciences = "CEDS.14",
-  PublicProtectiveAndGovernmentService = "CEDS.15",
-  HospitalityAndTourism = "CEDS.16",
-  ArchitectureAndConstruction = "CEDS.17",
-  AgricultureFoodAndNaturalResources = "CEDS.18",
-  HumanServices = "CEDS.19",
-  TransportationDistributionAndLogistics = "CEDS.20",
-  EngineeringAndTechnology = "CEDS.21",
-  Miscellaneous = "CEDS.22",
-  NonSubjectSpecific = "CEDS.23",
-  WorldLanguages = "CEDS.24",
+    // CEDS Subjects
+    LanguageArts = 'CEDS.01', // EnglishLanguageAndLiterature in CEDS
+    Mathematics = 'CEDS.02',
+    LifeAndPhysicalSciences = 'CEDS.03',
+    SocialSciencesAndHistory = 'CEDS.04',
+    VisualAndPerformingArts = 'CEDS.05',
+    ReligiousEducationAndTheology = 'CEDS.07',
+    PhysicalHealthAndSafetyEducation = 'CEDS.08',
+    MilitaryScience = 'CEDS.09',
+    InformationTechnology = 'CEDS.10',
+    CommunicationAndAudioVisualTechnology = 'CEDS.11',
+    BusinessAndMarketing = 'CEDS.12',
+    Manufacturing = 'CEDS.13',
+    HealthCareSciences = 'CEDS.14',
+    PublicProtectiveAndGovernmentService = 'CEDS.15',
+    HospitalityAndTourism = 'CEDS.16',
+    ArchitectureAndConstruction = 'CEDS.17',
+    AgricultureFoodAndNaturalResources = 'CEDS.18',
+    HumanServices = 'CEDS.19',
+    TransportationDistributionAndLogistics = 'CEDS.20',
+    EngineeringAndTechnology = 'CEDS.21',
+    Miscellaneous = 'CEDS.22',
+    NonSubjectSpecific = 'CEDS.23',
+    WorldLanguages = 'CEDS.24',
 
-  // Edlink Subjects
-  SpecialEducation = "EL.01",
-  ProfessionalDevelopment = "EL.02",
+    // Edlink Subjects
+    SpecialEducation = 'EL.01',
+    ProfessionalDevelopment = 'EL.02'
 }
 /**
  * @export
  * @enum {string}
  */
 export enum GradeLevel {
-  Birth = "Birth",
-  Prenatal = "Prenatal",
-  InfantToddler = "IT",
-  Preschool = "PR",
-  Prekindergarten = "PK",
-  TransitionalKindergarten = "TK",
-  Kindergarten = "KG",
-  FirstGrade = "01",
-  SecondGrade = "02",
-  ThirdGrade = "03",
-  FourthGrade = "04",
-  FifthGrade = "05",
-  SixthGrade = "06",
-  SeventhGrade = "07",
-  EighthGrade = "08",
-  NinthGrade = "09",
-  TenthGrade = "10",
-  EleventhGrade = "11",
-  TwelfthGrade = "12",
-  GradeThirteen = "13",
-  Postsecondary = "PS",
-  Ungraded = "UG",
-  Other = "Other",
+    Birth = 'Birth',
+    Prenatal = 'Prenatal',
+    InfantToddler = 'IT',
+    Preschool = 'PR',
+    Prekindergarten = 'PK',
+    TransitionalKindergarten = 'TK',
+    Kindergarten = 'KG',
+    FirstGrade = '01',
+    SecondGrade = '02',
+    ThirdGrade = '03',
+    FourthGrade = '04',
+    FifthGrade = '05',
+    SixthGrade = '06',
+    SeventhGrade = '07',
+    EighthGrade = '08',
+    NinthGrade = '09',
+    TenthGrade = '10',
+    EleventhGrade = '11',
+    TwelfthGrade = '12',
+    GradeThirteen = '13',
+    Postsecondary = 'PS',
+    Ungraded = 'UG',
+    Other = 'Other'
 }
 
 export interface Identifier {
-  /**
-   * @type {string}
-   * @memberof Identifier
-   */
-  type: string;
-  /**
-   * @type {string}
-   * @memberof Identifier
-   */
-  value: string;
+    /**
+     *
+     * @type {string}
+     * @memberof Identifier
+     */
+    type: string;
+    /**
+     *
+     * @type {string}
+     * @memberof Identifier
+     */
+    value: string;
 }
 
 //
@@ -83,36 +85,36 @@ export interface Identifier {
  * @enum {string}
  */
 export enum IdentifierType {
-  SIS = "sis_id",
-  State = "state_id",
-  School = "school_id",
-  Username = "username",
-  Email = "email",
-  LDAP = "ldap",
-  LTI = "lti",
+    SIS = 'sis_id',
+    State = 'state_id',
+    School = 'school_id',
+    Username = 'username',
+    Email = 'email',
+    LDAP = 'ldap',
+    LTI = 'lti',
 
-  Edlink = "edlink_id",
+    Edlink = 'edlink_id',
 
-  NCES = "nces_id",
-  MDR = "mdr_id",
-  IPEDS = "ipeds_id",
+    NCES = 'nces_id',
+    MDR = 'mdr_id',
+    IPEDS = 'ipeds_id',
 
-  Aeries = "aeries_id",
-  Blackbaud = "blackbaud_id",
-  Blackboard = "blackboard_id",
-  Brightspace = "brightspace_id",
-  Bromcom = "bromcom_id",
-  Canvas = "canvas_id",
-  CanvasLti = "canvas_lti_id",
-  Clever = "clever_id",
-  Google = "google_id",
-  Illuminate = "illuminate_id",
-  Microsoft = "microsoft_id",
-  Moodle = "moodle_id",
-  OneRoster = "oneroster_id",
-  PowerSchool = "powerschool_id",
-  Schoology = "schoology_id",
-  Skyward = "skyward_id",
+    Aeries = 'aeries_id',
+    Blackbaud = 'blackbaud_id',
+    Blackboard = 'blackboard_id',
+    Brightspace = 'brightspace_id',
+    Bromcom = 'bromcom_id',
+    Canvas = 'canvas_id',
+    CanvasLti = 'canvas_lti_id',
+    Clever = 'clever_id',
+    Google = 'google_id',
+    Illuminate = 'illuminate_id',
+    Microsoft = 'microsoft_id',
+    Moodle = 'moodle_id',
+    OneRoster = 'oneroster_id',
+    PowerSchool = 'powerschool_id',
+    Schoology = 'schoology_id',
+    Skyward = 'skyward_id'
 }
 
 /**
@@ -120,9 +122,9 @@ export enum IdentifierType {
  * @enum {string}
  */
 export enum Gender {
-  Male = "male",
-  Female = "female",
-  Other = "other",
+    Male = 'male',
+    Female = 'female',
+    Other = 'other'
 }
 
 /**
@@ -130,12 +132,11 @@ export enum Gender {
  * @enum {string}
  */
 export enum Race {
-  AmericanIndianOrAlaskaNative = "american-indian-or-alaska-native",
-  Asian = "asian",
-  BlackOrAfricanAmerican = "black-or-african-american",
-  NativeHawaiianOrOtherPacificIslander =
-    "native-hawaiian-or-other-pacific-islander",
-  White = "white",
+    AmericanIndianOrAlaskaNative = 'american-indian-or-alaska-native',
+    Asian = 'asian',
+    BlackOrAfricanAmerican = 'black-or-african-american',
+    NativeHawaiianOrOtherPacificIslander = 'native-hawaiian-or-other-pacific-islander',
+    White = 'white'
 }
 
 /**
@@ -143,144 +144,133 @@ export enum Race {
  * @enum {string}
  */
 export enum Role {
-  Student = "student",
-  DistrictAdministrator = "district-administrator",
-  Administrator = "administrator",
-  Teacher = "teacher",
-  TeachingAssistant = "ta",
-  Staff = "staff",
-  Aide = "aide",
-  Observer = "observer",
-  Parent = "parent",
-  Guardian = "guardian",
-  Designer = "designer",
-  Member = "member",
+    Student = 'student',
+    DistrictAdministrator = 'district-administrator',
+    Administrator = 'administrator',
+    Teacher = 'teacher',
+    TeachingAssistant = 'ta',
+    Staff = 'staff',
+    Aide = 'aide',
+    Observer = 'observer',
+    Parent = 'parent',
+    Guardian = 'guardian',
+    Designer = 'designer',
+    Member = 'member'
 }
 
 export enum TokenSetType {
-  Integration = "integration",
-  Person = "person",
-  Application = "application",
-}
+    Integration = 'integration',
+    Person = 'person',
+    Application = 'application'
+};
 
 export type BaseTokenSet = {
-  type: TokenSetType;
-  token_type?: "Bearer";
-  access_token: string;
-  refresh_token?: string;
-  context?: Record<string, any>;
+    type: TokenSetType;
+    token_type?: 'Bearer';
+    access_token: string;
+    refresh_token?: string;
+    context?: Record<string, any>;
 };
 
 /**
  * Important note: If you do not provide a refresh token the SDK will not be able to refresh the token for you and requests made an hour after the initial token exchange will fail
  */
 export type PersonTokenSet = BaseTokenSet & {
-  type: TokenSetType.Person;
-  access_token?: string;
-  refresh_token?: string;
-  expires_in?: Date;
-  expiration_date?: Date;
+    type: TokenSetType.Person;
+    access_token?: string;
+    refresh_token?: string;
+    expires_in?: Date;
+    expiration_date?: Date;
 };
 
 export type IntegrationTokenSet = BaseTokenSet & {
-  type: TokenSetType.Integration;
-  access_token: string;
+    type: TokenSetType.Integration;
+    access_token: string;
 };
 
-export type ApplicationTokenSet = BaseTokenSet & {
-  type: TokenSetType.Application;
-  access_token: string;
-};
-
-export type TokenSet =
-  | PersonTokenSet
-  | IntegrationTokenSet
-  | ApplicationTokenSet;
+export type TokenSet = PersonTokenSet | IntegrationTokenSet;
 
 export type RequestOptionsPaging = {
-  limit?: number;
-  filter?: Record<string, any>;
+    limit?: number;
+    filter?: Record<string, any>;
 } & RequestOptionsGet;
 
 export type RequestOptionsGet = {
-  expand?: string[];
+    expand?: string[];
 } & RequestOptionsBase;
 
 export type RequestOptionsPost = {
-  idempotency?: string;
+    idempotency?: string;
 } & RequestOptionsBase;
 
 export type RequestOptionsBase = {
-  properties?: string[];
+    properties?: string[];
 };
 
-export type RequestOptions =
-  | RequestOptionsPaging
-  | RequestOptionsGet
-  | RequestOptionsBase;
+export type RequestOptions = RequestOptionsPaging | RequestOptionsGet | RequestOptionsBase;
 
 export enum ProductState {
-  Active = "active",
-  Inactive = "inactive",
-  Upcoming = "upcoming",
-  Development = "development",
-  Sunsetting = "sunsetting",
-  Deprecated = "deprecated",
-}
+    Active = 'active',
+    Inactive = 'inactive',
+    Upcoming = 'upcoming',
+    Development = 'development',
+    Sunsetting = 'sunsetting',
+    Deprecated = 'deprecated'
+};
 
 export enum IntegrationState {
-  Requested = "requested",
-  Inactive = "inactive",
-  Active = "active",
-  Error = "error",
-  Disabled = "disabled",
-  Paused = "paused",
-  Pending = "pending",
-  Scheduled = "scheduled",
-}
+    Requested = 'requested',
+    Inactive = 'inactive',
+    Active = 'active',
+    Error = 'error',
+    Disabled = 'disabled',
+    Paused = 'paused',
+    Pending = 'pending',
+    Scheduled = 'scheduled'
+};
 
 export type Integration = {
-  state: IntegrationState;
-  id: string;
-  created_date: string;
-  updated_date: string;
-  permissions: string[];
-  scope: string;
-  start_date: string;
-  end_date: string;
-  locked: boolean;
-  application_id: string;
-  source_id: string;
-  destination_id: string;
-  region_id: string;
+    state: IntegrationState;
+    id: string;
+    created_date: string;
+    updated_date: string;
+    permissions: string[];
+    scope: string;
+    start_date: string;
+    end_date: string;
+    locked: boolean;
+    application_id: string;
+    source_id: string;
+    destination_id: string;
+    region_id: string;
 };
 
 export enum SourceState {
-  Inactive = "inactive", // Waiting to be validated (set when configuration changes)
-  Active = "active", // First sync was successful, scheduled to sync regularly
-  Error = "error", // Validation or sync failed
-  Disabled = "disabled", // Disabled manually
-  Destroyed = "destroyed", // Basically just waiting for cleanup.
-}
+    Inactive = 'inactive', // Waiting to be validated (set when configuration changes)
+    Active = 'active', // First sync was successful, scheduled to sync regularly
+    Error = 'error', // Validation or sync failed
+    Disabled = 'disabled', // Disabled manually
+    Destroyed = 'destroyed' // Basically just waiting for cleanup.
+};
 
 export type Source = {
-  state: SourceState;
-  id: string;
-  created_date: string;
-  updated_date: string;
-  name: string;
-  sync_interval: number;
-  provider_id: string;
-  properties: Record<string, any>;
-  team_id: string;
-  provider: {
-    active: boolean;
-    requires_administrator_login: boolean;
-    requires_administrator_consent: boolean;
-    requires_remote_configuration: boolean;
-    allows_data_sync: boolean;
+    state: SourceState;
     id: string;
+    created_date: string;
+    updated_date: string;
     name: string;
-    application: string;
-  };
+    sync_interval: number;
+    provider_id: string;
+    properties: Record<string, any>;
+    team_id: string;
+    provider: {
+        active: boolean;
+        requires_administrator_login: boolean;
+        requires_administrator_consent: boolean;
+        requires_remote_configuration: boolean;
+        allows_data_sync: boolean;
+        id: string;
+        name: string;
+        application: string;
+    }
 };
